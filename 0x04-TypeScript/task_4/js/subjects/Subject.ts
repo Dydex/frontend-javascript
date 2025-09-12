0e7b0e84017198c64d8e449eb94a52fa91d6b70a
Dydex <supremeajala@gmail.com>
2025-09-12 14:48:36 +0100

    first commit for task9

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,11 +1,12 @@
 // task_4/js/Subject.ts
 /// <reference path="./Teacher.ts" />
 
+// prettier-ignore
 namespace Subjects {
   export class Subject {
     teacher: Teacher;
 
-    set setTeacher(teacher: Teacher) {
+    set setTeacher(teacher:Teacher) {
       this.teacher = teacher;
     }
   }

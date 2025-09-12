010b8164f8876d59dfac5baf8027d6db0f282de0
Dydex <supremeajala@gmail.com>
2025-09-12 14:44:47 +0100

    first commit for task9

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,24 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requiremcents for Cpp';
+    }
+    getAvailableTeacher(): string {
+      if (
+        !this.teacher ||
+        this.teacher.experienceTeachingC === undefined ||
+        this.teacher.experienceTeachingC <= 0
+      ) {
+        return 'No available teacher';
+      }
+      return `Available Teacher: ${this.teacher.firstName}`;
+    }
+  }
+}

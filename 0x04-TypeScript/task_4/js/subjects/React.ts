1f6fbb1be37115a31b812fcf347cd6ffd7b15f0a
Dydex <supremeajala@gmail.com>
2025-09-12 14:53:29 +0100

    first commit for task9

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -3,19 +3,19 @@
 
 namespace Subjects {
   export interface Teacher {
-    experienceTeachingC?: number;
+    experienceTeachingReact?: number;
   }
 
   export class React extends Subject {
     getRequirements(): string {
-      return 'Here is the list of requirements for Cpp';
+      return 'Here is the list of requirements for React';
     }
 
     getAvailableTeacher(): string {
       if (
         !this.teacher ||
-        this.teacher.experienceTeachingC === undefined ||
-        this.teacher.experienceTeachingC <= 0
+        this.teacher.experienceTeachingReact === undefined ||
+        this.teacher.experienceTeachingReact <= 0
       ) {
         return 'No available teacher';
       }
